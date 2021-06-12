bf563d21fa5c7b0e0bec6bdd25fdd9a44a3a6d95
Chahat-M <chahatmittal710@gmail.com>
2021-06-12 17:58:12 +0530

    Update Compound_interest.py
    Added doc string

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,17 +1,40 @@
 import matplotlib.pyplot as plt
 
-def Compound_Interest(P,R,T,freq):
+def Compound_Interest(P :int,R :float,T :int,freq :str) -> None:
     
-    #CI = p *(1+r/100)**t
+    
+    """
+     Calculates and visualises compound interest 
+
+     Parameters:
+        P(float) : Principal amount
+        R(float) : Rate of interest
+        T(int) : Time
+        freq(str) : The frequency in which compound interest should be calculated. 
+                    The values can be from the list: ('annually','semiannually','quaterly','monthly')
+
+    Return:
+        This function does not return anything.
+    
+    Example:
+       Compound_Interest(20000,4.5,5,'quaterly')
+        
+    """
+
+
+    # defining required lists
     interest = []
     time = []
     Amount = []
+    deposit=P
+    TI=0
     
-    # compound interest calculation with graphical representation
+    # compound interest calculation along with graphical representation 
     if(freq=='monthly'):
         for i in range(12*T):
             I = P * R/1200
             P = P + I
+            TI+=I
             print("Interest recieved in month {} is {}".format(i+1,I))
             interest.append(I)
             time.append(i+1)
@@ -22,10 +45,20 @@ def Compound_Interest(P,R,T,freq):
         plt.ylabel("Amount")
         plt.show()
         
+        x=[]
+        x.append(deposit)
+        x.append(TI)
+        names=["Deposit","Interest"]
+        depexplode=[0,0.2]
+        plt.pie(x,labels=names,explode=depexplode , shadow=True)
+        plt.legend(title="Ratios:")
+        plt.show()  
+        
     elif (freq == 'annually'):
         for i in range(T):
             I = P * R/100
             P = P + I
+            TI+=I
             print("Interest recieved in year {} is {}".format(i+1,I))
             interest.append(I)
             time.append(i+1)
@@ -35,11 +68,21 @@ def Compound_Interest(P,R,T,freq):
         plt.xlabel("Years")
         plt.ylabel("Amount")
         plt.show()
+         
+        x=[]
+        x.append(deposit)
+        x.append(TI)
+        names=["Deposit","Interest"]
+        depexplode=[0,0.2]
+        plt.pie(x,labels=names,explode=depexplode , shadow=True)
+        plt.legend(title="Ratios:")
+        plt.show() 
         
     elif (freq == 'quaterly'):
         for i in range(4*T):
             I = P * R/400
             P = P + I
+            TI+=I
             print("Interest recieved in quarter year {} is {}".format(i+1,I))
             interest.append(I)
             time.append(i+1)
@@ -50,10 +93,20 @@ def Compound_Interest(P,R,T,freq):
         plt.ylabel("Amount")
         plt.show()
         
+        x=[]
+        x.append(deposit)
+        x.append(TI)
+        names=["Deposit","Interest"]
+        depexplode=[0,0.2]
+        plt.pie(x,labels=names,explode=depexplode , shadow=True)
+        plt.legend(title="Ratios:")
+        plt.show()
+        
     elif(freq == 'semiannually'):
         for i in range(2*T):
             I = P * R/200
             P = P + I
+            TI+=I
             print("Interest recieved in semi year {} is {}".format(i+1,I))
             interest.append(I)
             time.append(i+1)
@@ -63,17 +116,7 @@ def Compound_Interest(P,R,T,freq):
         plt.xlabel("Semi Years")
         plt.ylabel("Amount")
         plt.show()
-
-# Displaying the calculated amount and interest in pie chart
-def Return_Percent(P,R,T,freq):
-    deposit=P
-    TI=0
-    
-    if(freq == 'annually'):
-        for i in range(T):
-            I=P*R/100
-            P=P+I
-            TI+=I
+        
         x=[]
         x.append(deposit)
         x.append(TI)
@@ -82,53 +125,3 @@ def Return_Percent(P,R,T,freq):
         plt.pie(x,labels=names,explode=depexplode , shadow=True)
         plt.legend(title="Ratios:")
         plt.show() 
-        
-    elif(freq == 'monthly'):
-        for i in range(12*T):
-            I=P*R/1200
-            P=P+I
-            TI+=I
-        x=[]
-        x.append(deposit)
-        x.append(TI)
-        names=["Deposit","Interest"]
-        depexplode=[0,0.2]
-        plt.pie(x,labels=names,explode=depexplode , shadow=True)
-        plt.legend(title="Ratios:")
-        plt.show()  
-        
-    elif(freq == 'semiannually'):
-        for i in range(2*T):
-            I=P*R/200
-            P=P+I
-            TI+=I
-        x=[]
-        x.append(deposit)
-        x.append(TI)
-        names=["Deposit","Interest"]
-        depexplode=[0,0.2]
-        plt.pie(x,labels=names,explode=depexplode , shadow=True)
-        plt.legend(title="Ratios:")
-        plt.show()  
-        
-    elif(freq == 'quaterly'):
-        for i in range(T):
-            I=P*R/100
-            P=P+I
-            TI+=I
-        x=[]
-        x.append(deposit)
-        x.append(TI)
-        names=["Deposit","Interest"]
-        depexplode=[0,0.2]
-        plt.pie(x,labels=names,explode=depexplode , shadow=True)
-        plt.legend(title="Ratios:")
-        plt.show()  
-        
-P=float(input("Enter the deposited amount : "))
-R=float(input("Enter the rate of interest per annum in percentage : "))
-T=int(input("Enter the term(years) : "))
-freq = input("Enter the frequency of compounding the interest:")
-x=Compound_Interest(P,R,T,freq)
-y=Return_Percent(P,R,T,freq)
-    
